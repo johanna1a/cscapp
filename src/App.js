823b7f947e84c97cfed299bdc583eabf210f7829
luiz <luiz.gonzalez6194@gmail.com>
2024-07-08 21:10:32 -0400

    fixed styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
 import {withAuthenticator} from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css';
+import React from 'react';
 
 
 Amplify.configure(awsExports);
@@ -11,9 +12,8 @@ Amplify.configure(awsExports);
 function App({signOut, user}) {
   return (
     <>
-<button onClick={signOut}>Sign Out</button>
 
-<ChatContainer />
+<ChatContainer signOut={signOut}/>
 
     </>
   );
